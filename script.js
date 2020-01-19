4515fc83f4635921ce71bafa6cd9108b06a710bf
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-01-18 22:54:44 -0500

    added copy to clipboard

--- script.js
+++ script.js
@@ -20,16 +20,33 @@ function copyToClipboard() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);*/
 // BONUS EVENT LISTENER
-
+var copy = document.getElementById("copy");
+var password = document.getElementById("password");
 
 function writePassword(max, min) {
-    var passChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
-    var passLength = Math.floor(Math.random() * (max - min + 1)) + min;
-    var randPassword = Array(passLength).fill(passChars).map(function (x) { return x[Math.floor(Math.random() * x.length)] }).join('');
-    return randPassword;
+  var passChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@!%&()/";
+  var passLength = Math.floor(Math.random() * (max - min + 1)) + min;
+  var randPassword = Array(passLength).fill(passChars).map(function (x) {
+    return x[Math.floor(Math.random() * x.length)]
+  }).join('');
+
+  return randPassword;
 }
 document.getElementById("generate").addEventListener("click", function () {
-    randomPassword = writePassword(16, 8);
-    document.getElementById("password").value = randomPassword;
+  randomPassword = writePassword(16, 8);
+  document.getElementById("password").value = randomPassword;
+
+});
+
+
+
+
+
+document.getElementById("copy").addEventListener("click", function() {
+  let textArea = document.getElementById("password");
+  password.select();
+  document.execCommand("copy");
+
+
 });
 
