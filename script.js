81bee3d5c5c535f4f085cf1f89600091354f1e9f
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-01-19 13:15:19 -0500

    removed comments

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-/* Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
-}
-
-function copyToClipboard() {
-  // BONUS 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);*/
-// BONUS EVENT LISTENER
 var copy = document.getElementById("copy");
 var password = document.getElementById("password");
 
